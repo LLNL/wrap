217b2c5e152683d4dda3374b5c2a3ac3ff436150
Alexander Haase <alexander.haase@rwth-aachen.de>
2016-01-20 12:10:14 +0100

    Added function declaration for get_mpi_f_status_size___

--- wrap.py
+++ wrap.py
@@ -125,7 +125,8 @@ _EXTERN_C_ void *MPIR_ToPointer(int);
 #if defined(MPI_STATUS_SIZE) && MPI_STATUS_SIZE > 0
 #define MPI_F_STATUS_SIZE MPI_STATUS_SIZE
 #else
-inline int __get_f_status_size(){int size; get_mpi_f_status_size___(&size); return size;}
+void get_mpi_f_status_size___(int*);
+static inline int __get_f_status_size(){int size; get_mpi_f_status_size___(&size); return size;}
 #define MPI_F_STATUS_SIZE __get_f_status_size()
 #endif
 
